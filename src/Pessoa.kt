9a4388a44a769c25b71cdfa131b682a6bc540ba8
Thaila52 <thaila.grag@gmail.com>
2021-03-12 01:16:45 -0300

    Initial commit

---
+++ src/Pessoa.kt
@@ -0,0 +1,14 @@
+
+
+class Pessoa {
+    var nome: String = "Thaila"
+    var cpf: String = "330.541.868-06"
+}
+fun main(){
+    val Thaila = Pessoa()
+    println(Thaila.nome)
+    println(Thaila.cpf)
+}
+
+
+
