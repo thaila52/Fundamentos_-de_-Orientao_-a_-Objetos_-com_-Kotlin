9a3c680ed094c765ce4f0dfd9798416c4c0dd8b7
Thaila52 <thaila.grag@gmail.com>
2021-03-12 14:40:37 -0300

    update  to apply private modifier to set function of cpf property

--- src/Pessoa.kt
+++ src/Pessoa.kt
@@ -1,14 +1,18 @@
 
-
 class Pessoa {
     var nome: String = "Thaila"
     var cpf: String = "330.541.868-06"
+    private set
+
 }
-fun main(){
+fun main() {
+
     val Thaila = Pessoa()
+    Thaila.cpf ="000"
     println(Thaila.nome)
     println(Thaila.cpf)
 }
+
 
 
 
