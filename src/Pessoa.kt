d04d82efbba55ba6bee8810a1860ca1b1dac8498
Thaila52 <thaila.grag@gmail.com>
2021-03-12 15:14:42 -0300

    update to talk about class members

--- src/Pessoa.kt
+++ src/Pessoa.kt
@@ -4,13 +4,13 @@ class Pessoa {
     var cpf: String = "330.541.868-06"
     private set
 
+    constructor()
+    fun pessoainfo() = "$nome e $cpf"
 }
 fun main() {
 
-    val Thaila = Pessoa()
-    Thaila.cpf ="000"
-    println(Thaila.nome)
-    println(Thaila.cpf)
+    val thaila = Pessoa()
+    println(thaila.pessoainfo())
 }
 
 
