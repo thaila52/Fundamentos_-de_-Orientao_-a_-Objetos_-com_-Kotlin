3e1ffd819cc449d59b6c75c0276065e347fb539a
Thaila52 <thaila.grag@gmail.com>
2021-03-15 18:37:38 -0300

    created sb classes manager and analyst

---
+++ src/Analista.kt
@@ -0,0 +1,12 @@
+
+
+class Analista (
+    nome: String,
+    cpf: String,
+    salario: Double
+) : Funcionario(nome, cpf, salario) {
+    override fun calculoAuxilio() = salario * 0.1
+
+
+
+}
---
+++ src/Grerente.kt
@@ -0,0 +1,8 @@
+class Gerente(
+    nome: String,
+    cpf: String,
+    salario: Double
+): Funcionario(nome = nome, cpf = cpf, salario = salario) {
+    override fun calculoAuxilio(): Double = salario * 0.4
+
+}
